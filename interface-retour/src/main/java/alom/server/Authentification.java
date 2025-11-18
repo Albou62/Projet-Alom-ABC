06aaf71ed1af05c067d288579e870d973c66e19c
CyrilGodet <cyril.godet.etu@univ-lille.fr>
2025-11-18 14:18:51 +0100

    ajout classe pour authentification dans interface retour

---
+++ interface-retour/src/main/java/alom/server/Authentification.java
@@ -0,0 +1,28 @@
+package alom.server;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "authentification" path)
+ */
+@Path("authentification")
+public class Authentification {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        App.inscriptionToken(token, nickname);
+        return "Got it!";
+    }
+}
+
+
