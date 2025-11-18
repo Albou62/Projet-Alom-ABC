dadd3f58e981e0cfb84eff56707a99df50f09a6e
CyrilGodet <cyril.godet.etu@univ-lille.fr>
2025-11-18 17:24:10 +0100

    modification pom microservice message

--- interface-aller/src/main/java/alom/server/InterfaceAller.java
+++ interface-aller/src/main/java/alom/server/InterfaceAller.java
@@ -68,5 +68,31 @@ public class InterfaceAller {
 		}
 	}
 
+	@POST
+	@Path("message")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response message(String jsonData) {
+		try {
+			Client client = ClientBuilder.newClient();
+			Response response = client.target(AUTHENTIFICATION_SERVICE + "/authentification/inscription")
+									  .request(MediaType.APPLICATION_JSON)
+									  .post(Entity.json(jsonData));
+			
+			String result = response.readEntity(String.class);
+			int status = response.getStatus();
+			response.close();
+			client.close();
+			
+			return Response.status(status).entity(result).build();
+		} 
+
+        catch (Exception e) {
+			return Response.status(500)
+						  .entity("{\"erreur\": \"Service message indisponible\"}")
+						  .build();
+		}
+	}
+
 
 }
